19f26df44f91b1f876e13115e6990f81ff5bf800
Madhuvarshini-Venkatachalam <madhuvarshinivenkatachalam@gmail.com>
2026-04-07 09:27:03 +0530

    UC12: Safety check using functional interface and lambda

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,9 @@
-import java.util.Scanner;
-import java.util.regex.*;
+import java.util.*;
+
+@FunctionalInterface
+interface SafetyCheck {
+    boolean check(String cargoType);
+}
 
 public class TrainConsistManagementApp {
 
@@ -7,33 +11,18 @@ public class TrainConsistManagementApp {
 
         Scanner sc = new Scanner(System.in);
 
-        // Patterns
-        String trainPattern = "TRN-\\d{4}";
-        String cargoPattern = "CG-[A-Z]{3}\\d{2}";
+        // Lambda for safety rule
+        SafetyCheck rule = (cargo) -> {
+            return !cargo.equalsIgnoreCase("Explosive");
+        };
 
-        System.out.print("Enter Train ID (Format: TRN-1234): ");
-        String trainId = sc.nextLine();
+        System.out.print("Enter Cargo Type: ");
+        String cargo = sc.nextLine();
 
-        System.out.print("Enter Cargo Code (Format: CG-ABC12): ");
-        String cargoCode = sc.nextLine();
-
-        // Compile patterns
-        Pattern p1 = Pattern.compile(trainPattern);
-        Pattern p2 = Pattern.compile(cargoPattern);
-
-        Matcher m1 = p1.matcher(trainId);
-        Matcher m2 = p2.matcher(cargoCode);
-
-        if (m1.matches()) {
-            System.out.println("✅ Valid Train ID");
+        if (rule.check(cargo)) {
+            System.out.println("✅ Safe Cargo - Allowed");
         } else {
-            System.out.println("❌ Invalid Train ID");
-        }
-
-        if (m2.matches()) {
-            System.out.println("✅ Valid Cargo Code");
-        } else {
-            System.out.println("❌ Invalid Cargo Code");
+            System.out.println("❌ Unsafe Cargo - Not Allowed");
         }
 
         sc.close();
