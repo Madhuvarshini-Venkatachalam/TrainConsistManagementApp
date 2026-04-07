0bfb638ce6e1addaef3c187bc893ead09ac30421
Madhuvarshini-Venkatachalam <madhuvarshinivenkatachalam@gmail.com>
2026-04-07 10:07:30 +0530

    UC20: Exception handling during search + test cases

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,32 +1,18 @@
-import java.util.Arrays;
+import java.util.List;
 
-public class UC19BinarySearch {
+public class UC20SearchException {
 
-    public static int binarySearch(String[] bogies, String target) {
-        int left = 0, right = bogies.length - 1;
+    public static boolean searchBogie(List<String> bogies, String target) {
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int cmp = bogies[mid].compareTo(target);
-
-            if (cmp == 0) return mid;
-            else if (cmp < 0) left = mid + 1;
-            else right = mid - 1;
+        if (bogies.isEmpty()) {
+            throw new RuntimeException("Train is empty");
         }
-        return -1;
-    }
 
-    public static void main(String[] args) {
-        String[] bogies = {"BG101", "BG102", "BG103", "BG104"};
-        Arrays.sort(bogies); // must be sorted
-
-        String target = "BG103";
-
-        int result = binarySearch(bogies, target);
-
-        if (result != -1)
-            System.out.println("Bogie Found at index: " + result);
-        else
-            System.out.println("Bogie Not Found");
+        for (String b : bogies) {
+            if (b.equals(target)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
